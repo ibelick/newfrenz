551f544bd93288aa5973ca5eb12a91f0febacda5
smoogy <dr.smoogy@gmail.com>
2022-04-13 01:12:05 +0200

    added connected wallet header

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,4 +1,5 @@
 import Link from "next/Link";
+import { useConnect, useAccount, Connector, ConnectorData } from "wagmi";
 
 interface LayoutProps {
   card: JSX.Element;
@@ -7,6 +8,14 @@ interface LayoutProps {
 }
 
 const Layout: React.FC<LayoutProps> = ({ isLoading, card, text }) => {
+  const [{ data: connectData, loading: isLoadingConnectData }, connect] =
+    useConnect();
+  const [{ data: accountData, loading: isLoadingAccountData }, disconnect] =
+    useAccount({
+      fetchEns: true,
+    });
+  const isUserNotConnected = Boolean(!connectData.connected && !accountData);
+
   return (
     <div className="bg-zinc-900 min-h-screen text-white pt-8 pb-12 font-IBM bg-[url('../public/bg-gradient.svg')]">
       <div className="h-1 w-48 bg-indigo-600 absolute top-0"></div>
@@ -17,8 +26,15 @@ const Layout: React.FC<LayoutProps> = ({ isLoading, card, text }) => {
               <a>→ ONBOARDING</a>
             </Link>
           </div>
-          <div>
-            <span>Wallet connected</span>
+          <div className="text-sm">
+            {isUserNotConnected ? (
+              <span></span>
+            ) : (
+              <div className="flex items-center">
+                <div className="h-2 mr-2 w-2 bg-green-500  border-green-300 rounded-full"></div>
+                Wallet connected
+              </div>
+            )}
           </div>
         </header>
         <div className="mt-24 lg:flex ">
