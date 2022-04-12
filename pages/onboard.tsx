256134d0d815c58647bf74e3bd53c452ebc7d007
ibelick <julien.thibeaut@gmail.com>
2022-04-12 09:24:35 +0200

    init onboard page

---
+++ pages/onboard.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Onboard = () => {
+  return (
+    <div>
+      <h1 className="text-xl">onboard</h1>
+      <div className="mt-2">
+        <span>hi</span>
+      </div>
+    </div>
+  );
+};
+
+export default Onboard;
