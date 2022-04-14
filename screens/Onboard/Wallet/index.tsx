d1fedf1ae745325a559dbb392fdc3ab867bc2bc8
smoogy <dr.smoogy@gmail.com>
2022-04-14 19:15:37 +0200

    added icons

--- screens/Onboard/Wallet/index.tsx
+++ screens/Onboard/Wallet/index.tsx
@@ -66,7 +66,7 @@ const NoWallet: React.FC<NoWalletProps> = ({
   return (
     <div>
       <div className="text-center">
-        <span className="text-3xl">⚉</span>
+        <img src="/connect.svg" className="w-16 m-auto mt-8 mb-4" />
         <h2 className="text-xl font-bold mt-8">Let's connect your wallet!</h2>
         <p className="text-gray-400 mt-2 mb-8">
           Now that you have installed Metamask and created an account you can
