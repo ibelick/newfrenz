29eed6bf8e802e106dd86c7b969279c47f28413e
ibelick <julien.thibeaut@gmail.com>
2022-04-15 08:45:39 +0200

    add meta, description

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -12,8 +12,38 @@ const Header = () => {
   return (
     <>
       <Head>
-        <title>newfrenz</title>
+        <title>newfrenz • onboard your frenz in web3 with a simple link</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta
+          name="title"
+          content="newfrenz • onboard your frenz in web3 with a simple link"
+        />
+        <meta
+          name="description"
+          content="onboard your frenz in web3 with a simple link "
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.newfrenz.xyz/" />
+        <meta
+          property="og:title"
+          content="newfrenz • onboard your frenz in web3 with a simple link"
+        />
+        <meta
+          property="og:description"
+          content="onboard your frenz in web3 with a simple link "
+        />
+        <meta property="og:image" content="/banner_social.jpg" />
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://www.newfrenz.xyz/" />
+        <meta
+          property="twitter:title"
+          content="newfrenz • onboard your frenz in web3 with a simple link"
+        />
+        <meta
+          property="twitter:description"
+          content="onboard your frenz in web3 with a simple link "
+        />
+        <meta property="twitter:image" content="/banner_social.jpg" />
       </Head>
       <header className="flex items-center justify-between mb-24">
         <div>
